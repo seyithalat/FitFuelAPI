ee37201fc620db355594ff682ac973fc07b78c6c
seyithalat <r1059309@student.thomasmore.be>
2025-11-05 16:53:13 +0100

    Initial commit - Fitness & Nutrition API
    f it says there are more conflicts, fix them, then repeat:
    
    git push -u origin main# Conflicts:
    
    git push -u origin main

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var express = require('express'); 
 var router = express.Router();
 
@@ -7,13 +6,3 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
-=======
-var express = require('express'); 
-var router = express.Router();
-
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'FitFuel v1.0.0' }); // Je eigen titel
-});
-
-module.exports = router;
->>>>>>> 558214c (Initial commit - Fitness & Nutrition API)
